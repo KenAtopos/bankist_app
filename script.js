24a740921ee670407d2ff239f33b415e75a38174
Ken Atopos <z1158229761@outlook.com>
2023-04-13 17:56:48 +0100

    add hover blur animation

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav");
 
 const openModal = function (event) {
   event.preventDefault();
@@ -76,10 +80,6 @@ document
   });
 
 // tabbed component
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
-
 tabsContainer.addEventListener("click", (event) => {
   const clicked = event.target.closest(".operations__tab");
   // guard clause
@@ -97,6 +97,25 @@ tabsContainer.addEventListener("click", (event) => {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+// menu fade animation
+const handleHover = function (event) {
+  if (event.target.classList.contains("nav__link")) {
+    const link = event.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((element) => {
+      if (element !== link) {
+        element.style.opacity = this;
+      }
+      logo.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
 
 ///////////////////////
 // const h1 = document.querySelector("h1");
